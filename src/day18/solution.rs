d4759cf178bc0b712144741164e65306821a207e
Ric Hard <mereep@gmx.net>
2020-12-18 20:38:22 +0100

    some comments and simplifications

--- src/day18/solution.rs
+++ src/day18/solution.rs
@@ -5,9 +5,13 @@ pub fn task1(with_operator_precedence: bool) {
     let data = get_input_data();
     let lines = str_to_lines(data);
 
-    let results: Vec<i64> = lines.into_iter().map(|line| compute_formula(&line, with_operator_precedence)).collect();
-    println!("Sum of results {}", results.into_iter().sum::<i64>());
+    let results: Vec<i64> = lines
+        .into_iter()
+        .map(|line| compute_formula(&line, with_operator_precedence))
+        .collect();
 
+    println!("Results: {:?} (with precendece={:?})", results, with_operator_precedence);
+    println!("Sum of results {} (with precendece={:?})", results.into_iter().sum::<i64>(), with_operator_precedence);
 }
 
 
@@ -15,24 +19,36 @@ pub fn task2() {
     task1(true);
 }
 
+/// Inicates what we are doing right now
 enum State {
-    ReadsLeftOperant,
-    ReadsRightOperant,
+    ReadsLeftOperand,
+    ReadsRightOperand,
     ReadsOperator
 }
 
+/// computes the equation
+///
+/// if [with_operator_precedence] is [true] we will execute + before *
+/// otherwise all operators are treated equal
 pub fn compute_formula(formula: &str, with_operator_precedence: bool) -> i64 {
+
+    // Read position
     let mut idx = 0 as usize;
 
-    let mut state = State::ReadsLeftOperant;
+    // Context of our `machine´
+    let mut state = State::ReadsLeftOperand;
+
+    // just reads an integer
     let regex_integer = Regex::new(r"\d+").expect("Integer regex not compileable");
     let mut curr_left_value: i64 = 0;
     let mut curr_operator: char = ' ';
-
     loop {
+        // Read the character and just break if we're done
         let maybe_curr_char = formula.chars().nth(idx);
         if maybe_curr_char.is_none() {break};
         let curr_char = maybe_curr_char.unwrap();
+
+        // Stores a value which was read in this step
         let mut read_value: i64= 0;
 
         // println!("Current idx {}: {}", idx, curr_char);
@@ -43,21 +59,22 @@ pub fn compute_formula(formula: &str, with_operator_precedence: bool) -> i64 {
                 idx = inner_part_end_idx + 1;
                 read_value = inner_result;
             },
-            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+            '0' ..= '9' => {
                 let number_match = regex_integer.captures(&formula[idx..])
                     .expect(&format!("Couldn't parse number beginning from index {}", idx));
 
-                read_value = *(&number_match[0].parse::<i64>()
-                    .expect(&format!("Couldn't parse number {} as integer", &number_match[0])));
+                read_value = number_match[0].parse::<i64>()
+                    .expect(&format!("Couldn't parse number {} as integer", &number_match[0]));
                 // println!("Read value parse {}", read_value);
                 let match_size = number_match[0].len();
                 idx += match_size;
             },
-            ' ' => {
-                idx += 1; continue
+            ' ' => { // White spaces are just eaten
+                idx += 1;
+                continue
             },
             '+'  => {
-                curr_operator = curr_char.clone();
+                curr_operator = curr_char;
             },
             '*' => {
                 if with_operator_precedence {
@@ -74,13 +91,13 @@ pub fn compute_formula(formula: &str, with_operator_precedence: bool) -> i64 {
 
 
         match state {
-            State::ReadsLeftOperant => {
+            State::ReadsLeftOperand => {
                 // println!("read value {}", read_value);
                 curr_left_value = read_value;
                 state = State::ReadsOperator;
             },
 
-            State::ReadsRightOperant => {
+            State::ReadsRightOperand => {
                 curr_left_value = match curr_operator {
                     '+' => curr_left_value + read_value,
                     '-' => curr_left_value - read_value,
@@ -91,7 +108,7 @@ pub fn compute_formula(formula: &str, with_operator_precedence: bool) -> i64 {
                 state = State::ReadsOperator;
             },
             State::ReadsOperator => {
-                state = State::ReadsRightOperant;
+                state = State::ReadsRightOperand;
             }
         }
         idx+=1;
@@ -100,7 +117,7 @@ pub fn compute_formula(formula: &str, with_operator_precedence: bool) -> i64 {
     return curr_left_value;
 }
 
-/// Reads until it finds a closing bracket )
+/// Reads until it finds a closing bracket `)´
 ///
 /// returns position BEFORE closing bracket
 fn read_until_matching_bracket(formula_part: &str, start_idx: usize) -> usize {
